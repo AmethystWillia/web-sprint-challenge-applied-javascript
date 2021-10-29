ea71d09e3ac4e08949dba3ee42e7cc8777f60e25
Amethyst Williams <zmithic@gmail.com>
2021-10-29 14:03:37 -0500

    Completed Task 4 - tabsAppender. Used several copies of code to achieve, now working on turning that into loops/forEachs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -18,22 +20,30 @@ const Tabs = (topics) => {
   const tab0Div = document.createElement('div');
   const tab1Div = document.createElement('div');
   const tab2Div = document.createElement('div');
+  const tab3Div = document.createElement('div');
+  const tab4Div = document.createElement('div');
 
   //Create hierarchy
   topicsDiv.appendChild(tab0Div);
   topicsDiv.appendChild(tab1Div);
   topicsDiv.appendChild(tab2Div);
+  topicsDiv.appendChild(tab3Div);
+  topicsDiv.appendChild(tab4Div);
 
   //Add content
   tab0Div.textContent = topics[0];
   tab1Div.textContent = topics[1];
   tab2Div.textContent = topics[2];
+  tab3Div.textContent = topics[3];
+  tab4Div.textContent = topics[4];
 
   //Adding classes
   topicsDiv.classList.add('topics')
   tab0Div.classList.add('tab');
   tab1Div.classList.add('tab');
   tab2Div.classList.add('tab');
+  tab3Div.classList.add('tab');
+  tab4Div.classList.add('tab');
 
   //Return main div
   return topicsDiv;
@@ -48,7 +58,25 @@ const tabsAppender = (selector) => {
   // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+  
+  //Set constant for the div everything will be slapped into
+  const tabsDiv = document.querySelector(selector)
+
+  axios.get(`http://localhost:5000/api/topics`)
+    .then(res => {
+      //Take array and slap it into the Tabs function
+      const tabsArray = Tabs(res.data.topics);
+
+      //Append tabsArray to tabsDiv
+      tabsDiv.appendChild(tabsArray);
+
+    })
+    .catch(err => {
+      console.error(err);
+    })
+
 }
+
+tabsAppender('.tabs-containuer');
 
 export { Tabs, tabsAppender }
