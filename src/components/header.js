28b1a66662de3218dec7a31ccff73f836cac8253
Amethyst Williams <zmithic@gmail.com>
2021-10-29 13:27:13 -0500

    Deleted an auto-import at the top of header.js and now the code is working

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,3 @@
-import { typeParameterDeclaration } from "@babel/types";
 
 const Header = (title, date, temp) => {
   // TASK 1
@@ -15,7 +14,7 @@ const Header = (title, date, temp) => {
   
   //Create elements
   const headerDiv = document.createElement('div');
-  const dateSpan = docuement.createElement('span');
+  const dateSpan = document.createElement('span');
   const titleH1 = document.createElement('h1');
   const tempSpan = document.createElement('span');
 
@@ -39,13 +38,20 @@ const Header = (title, date, temp) => {
 
 }
 
+//Test
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header('bruh', 'test', 'foo'));
+
 const headerAppender = (selector) => {
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
-  //
+  
+  
+
+
 }
 
 export { Header, headerAppender }
